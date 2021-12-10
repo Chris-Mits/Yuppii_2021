c33717d62c92905a56e38500c0670cc54a28b9f6
Χρήστος Μητσιάρης <chrismits88@gmail.com>
2021-12-11 01:18:32 +0200

    Made changes to openweather source, and some changes in home winter website

--- winter_edition/js/weather/ui.js
+++ winter_edition/js/weather/ui.js
@@ -1,17 +1,16 @@
 class UI {
 	constructor() {
-		this.location = document.getElementById('w-location');
 		this.desc = document.getElementById('w-desc');
 		this.string = document.getElementById('w-string');
 		this.icon = document.getElementById('w-icon');
-		this.details = document.getElementById('w-details');
-		this.humidity = document.getElementById('w-humidity');
 		this.feelsLike = document.getElementById('w-feels-like');
-		this.dewpoint = document.getElementById('w-dewpoint');
-		this.wind = document.getElementById('w-wind');
+		this.humidity = document.getElementById('w-humidity');
+		// this.location = document.getElementById('w-location');
+		// this.details = document.getElementById('w-details');
+		// this.dewpoint = document.getElementById('w-dewpoint');
+		// this.wind = document.getElementById('w-wind');
 	}
 	paint(weather) {
-		// this.location.textContent = weather.name;
 		this.desc.textContent = weather.weather[0].description;
 		this.string.textContent = weather.main.temp + '°C';
 		this.icon.setAttribute(
@@ -20,6 +19,7 @@ class UI {
 		);
 		this.feelsLike.textContent = `Αίσθηση: ${weather.main.feels_like}°C`;
 		this.humidity.textContent = `Υγρασία: ${weather.main.humidity}%`;
+		// this.location.textContent = weather.name;
 		// this.wind.textContent = `Ταχύτητα ανέμου: ${weather.wind.speed} m/s`;
 		// this.dewpoint.textContent = `Κατεύθυνση ανέμου: ${weather.wind.deg} μοίρες`;
 	}
--- winter_edition/js/weather/weather.js
+++ winter_edition/js/weather/weather.js
@@ -6,13 +6,15 @@ class Weather {
 	}
 	// Fetch weather from API
 	async getWeather() {
-		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric&lang=el`);
+		const response = await fetch(`
+			https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric&lang=el
+		`);
 		const responseData = await response.json();
 		return responseData;
 	}
 	// Change weather location
-	changeLocation(city, country) {
-		this.city = city;
-		this.country = country;
-	}
+	// changeLocation(city, country) {
+	// 	this.city = city;
+	// 	this.country = country;
+	// }
 }
